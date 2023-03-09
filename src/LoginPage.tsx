f64d50c51a33e3b9fa8c3c0782af6142e67b825d
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-03-09 21:50:57 +0600

    add login page

---
+++ src/LoginPage.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { observer } from 'mobx-react-lite';
+import { useMobxStore } from './MobxStoreProvider';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPage = observer(() => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const mobxStore = useMobxStore();
+
+  const navigate = useNavigate();
+
+  const handleLogin = (event: any) => {
+    event.preventDefault();
+
+    // Check if the entered credentials are valid
+    if (username === 'admin' && password === 'admin') {
+      // Store the authorization token in Mobx storage
+      mobxStore.updateAuthorized(true)
+      mobxStore.updateAuthorizationToken('my_token');
+      navigate('/')
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+});
+
+export default LoginPage;
