d215441d76c6459a7bb51a48eb5dfea0344cbc56
snurgaliev@cic.kz <snurgaliev@cic.kz>
2023-03-09 22:01:21 +0600

    checking for session token

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { useMobxStore } from "./MobxStoreProvider";
-
+import Cookies from "js-cookie";
 interface Props {
   children?: React.ReactNode;
 }
@@ -10,15 +10,25 @@ interface Props {
 const ProtectedRoute = observer(({ children }: Props) => {
   const mobxStore = useMobxStore();
 
+  const sessionToken = Cookies.get("session_token");
+
   let location = useLocation();
   const url = new URLSearchParams();
   url.set("redirect", location.pathname + location.search);
 
-  return mobxStore.token ? (
-    <>{children}</>
-  ) : (
-    <Navigate to={{ pathname: "/login", search: url.toString() }} />
-  );
+  if (sessionToken) {
+    return <>{children}</>;
+  }
+
+  if (mobxStore.token) {
+    return <>{children}</>;
+  }
+
+  if (!sessionToken && !mobxStore.token) {
+    return <Navigate to={{ pathname: "/login", search: url.toString() }} />;
+  }
+
+  return null;
 });
 
 export default ProtectedRoute;
